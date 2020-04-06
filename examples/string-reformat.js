f2fb0fdb20e120069cde12b32b8c4c53d165c9eb
danocmx <glencocomaster@centrum.cz>
2020-04-06 12:46:28 +0200

    Remove tests and examples

--- examples/string-reformat.js
+++
@@ -1,8 +0,0 @@
-// string_reformat.js
-
-const itemFormatting = require("../src/index");
-
-// Obviously this you will never need this kind of item parsed
-let item = "Festivized Strange Australium Killstreak Jazzy Attendant (Field-Tested) Kit";
-// Output -> Strange Jazzy Festivized Killstreak Australium Attendant Kit (Field-Tested) 
-item = itemFormatting.stringify( itemFormatting.parse(item) );
