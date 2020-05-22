b755fd6eac4ccb643479818d5e97577a88a68a8d
danocmx <glencocomaster@centrum.cz>
2020-05-22 18:25:46 +0200

    Fix tags bug

--- src/parseEconItem/ParsedEcon/getTags.js
+++ src/parseEconItem/ParsedEcon/getTags.js
@@ -32,12 +32,12 @@ module.exports = function ({ item }) {
 		const tag = tags[i];
 
 		// The position is saved.
-		if (isQuality(tag)) attributes.quality.push(getQuality(tag)); // diff
-		else if (isType(tag)) attributes.type = getType(tag);	// diff
-		else if (isClass(tag)) attributes.class = getClass(tag);
+		if (isClass(tag)) attributes.classes.push(getClass(tag));
+		else if (isType(tag)) attributes.type = getType(tag);
+		else if (isQuality(tag)) attributes.class = getQuality(tag);
 		else if (isGrade(tag)) attributes.grade = getGrade(tag);
 		else if (isCollection(tag)) attributes.collection = getCollection(tag);
-		else if (isWear(tag)) attributes.wear = getWear(tag); // diff
+		else if (isWear(tag)) attributes.wear = getWear(tag);
 	}
 
 	return attributes;
