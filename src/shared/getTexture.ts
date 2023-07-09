81c402c034852104f2eb926b85899c9e407e03d7
piman <44853490+piman51277@users.noreply.github.com>
2023-07-08 23:06:39 -0400

    Further optimize getTexture
    + Caches result of Object.keys() for increased performance
    + Strips unused items from search pool and caches the result
    
    Now provides ~40% boost for static and a ~100% boost for more optimized implementations. (For the parseEconItem function)

--- src/shared/getTexture.ts
+++ src/shared/getTexture.ts
@@ -1,8 +1,17 @@
 import isNumber from '../util/isNumber';
 
 import { ISchema } from '../types/schema';
+import { SchemaEnum } from 'tf2-static-schema';
 
 const TEXTURE_EXCEPTIONS = [['Health and Hell', 'Health and Hell (Green)']];
+
+//cache the textureKeys since Object.keys() is expensive
+let isTextureKeysCached = false;
+let textureKeys: string[] = [];
+
+//cache a stripped version of textures
+let isTexturesCached = false;
+let textures: SchemaEnum = {};
 
 /**
  * Iterates over effects object to get matching effect.
@@ -12,8 +21,24 @@ export default function (
 	name: string,
 	attributes: { wear: any | null; schema: ISchema }
 ): string | void {
-	const textures = attributes.schema.getTextures();
-	const textureKeys = Object.keys(textures);
+	if (!isTexturesCached) {
+		const copysrc = attributes.schema.getTextures();
+
+		//strip away all properties with numeric keys
+		//we must also copy this object, as we don't want to modify the original
+		const keys = Object.keys(copysrc);
+		for (const key of keys) {
+			if (!isNumber(key)) {
+				textures[key] = copysrc[key];
+			}
+		}
+
+		isTexturesCached = true;
+	}
+	if (!isTextureKeysCached) {
+		textureKeys = Object.keys(textures);
+		isTextureKeysCached = true;
+	}
 
 	for (let j = 0; j < TEXTURE_EXCEPTIONS.length; j++) {
 		const exception = TEXTURE_EXCEPTIONS[j];
@@ -25,7 +50,7 @@ export default function (
 	for (let i = 0; i < textureKeys.length; i++) {
 		const texture = textureKeys[i];
 
-		if (!name.includes(`${texture} `) || isNumber(texture)) {
+		if (!name.includes(`${texture} `)) {
 			// eslint-disable-next-line no-continue
 			continue;
 		}
