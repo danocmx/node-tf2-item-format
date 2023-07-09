ee2f5eb24c6ea55e0f443124663ee7385c2ea772
piman <44853490+piman51277@users.noreply.github.com>
2023-07-08 21:03:46 -0400

    Optimize getTexture func
    Minor optimization updates

--- src/shared/getTexture.ts
+++ src/shared/getTexture.ts
@@ -2,9 +2,7 @@ import isNumber from '../util/isNumber';
 
 import { ISchema } from '../types/schema';
 
-const TEXTURE_EXCEPTIONS = [
-	['Health and Hell', 'Health and Hell (Green)'],
-];
+const TEXTURE_EXCEPTIONS = [['Health and Hell', 'Health and Hell (Green)']];
 
 /**
  * Iterates over effects object to get matching effect.
@@ -16,25 +14,23 @@ export default function (
 ): string | void {
 	const textures = attributes.schema.getTextures();
 	const textureKeys = Object.keys(textures);
-	for (let i = 0; i < textureKeys.length; i++) {
-		const texture: number | string = textureKeys[i];
 
-		if (
-			texture === 'Haunted Ghosts' &&
-			name.includes('Haunted Ghosts') &&
-			!attributes.wear
-		) {
+	for (let j = 0; j < TEXTURE_EXCEPTIONS.length; j++) {
+		const exception = TEXTURE_EXCEPTIONS[j];
+		if (name.includes(`${exception[1]} `)) return exception[1];
+	}
+
+	const skipHauntedFlag = name.includes('Haunted Ghosts') && !attributes.wear;
+
+	for (let i = 0; i < textureKeys.length; i++) {
+		const texture = textureKeys[i];
+
+		if (!name.includes(`${texture} `) || isNumber(texture)) {
+			// eslint-disable-next-line no-continue
 			continue;
 		}
 
-		for (let j = 0; j < TEXTURE_EXCEPTIONS.length; j++) {
-			const exception = TEXTURE_EXCEPTIONS[j];
-			if (texture === exception[0] && name.includes(`${exception[1]} `))
-				return exception[1];
-		}
-
-		if (isNumber(texture) || !name.includes(`${texture} `)) {
-			// eslint-disable-next-line no-continue
+		if (texture === 'Haunted Ghosts' && skipHauntedFlag) {
 			continue;
 		}
 
