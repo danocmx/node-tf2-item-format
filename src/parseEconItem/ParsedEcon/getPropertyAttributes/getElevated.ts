13acb41ea348252336a9502f015402be43fc1f72
danocmx <glencocomaster@centrum.cz>
2025-10-22 21:47:13 +0200

    fix: strange elevated edge cases

--- src/parseEconItem/ParsedEcon/getPropertyAttributes/getElevated.ts
+++ src/parseEconItem/ParsedEcon/getPropertyAttributes/getElevated.ts
@@ -1,11 +1,19 @@
 import { EconItem, TagAttributes } from '../../../types';
 
 export default function (item: EconItem, tags: TagAttributes): boolean {
-	return isStrangeType(item) && !isStrangeQuality(tags);
+	return isStrange(item) && !isStrangeQuality(tags);
+}
+
+function isStrange(item: EconItem) {
+	if (item.type === '') { // Weird case for strange weapons
+		return item.market_hash_name.includes("Strange ");
+	}
+
+	return isStrangeType(item);
 }
 
 function isStrangeType({ type }: EconItem): boolean {
-	return /( - Kills: |- Points Scored: |- Carnival Kills: )\d+/.test(type);
+	return /( - Kills: |- Points Scored: |- Carnival Kills: | - Times Performed: )\d+/.test(type);
 }
 
 function isStrangeQuality({ quality }: TagAttributes): boolean {
