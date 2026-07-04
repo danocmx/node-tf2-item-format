e62459b84808db6cb36f5ada37bfdd812940a878
danocmx <glencocomaster@centrum.cz>
2026-07-04 17:31:09 +0200

    refactor: extra test cases into json files

--- test/fixName.js
+++ test/fixName.js
@@ -1,35 +1,17 @@
+const fs = require('fs');
+const path = require('path');
 const { assert } = require('chai');
 
-const { fixName } = require('../dist/static')
+const { fixName } = require('../dist/static');
 
-describe('fixName', () => {
-	it('Case #1', () => {
-		assert.equal(fixName('Strange Australium Festivized Professional Killstreak Scattergun'), 'Strange Festivized Professional Killstreak Australium Scattergun');
-	})
+const cases = JSON.parse(
+	fs.readFileSync(path.join(__dirname, './data/fixName.json'), 'utf8')
+);
 
-	it('Case #2', () => {
-		assert.equal(fixName('Omniscient Orb Strange Balloonihoodie'), 'Strange Omniscient Orb Balloonihoodie');
-	})
+describe('fixName data', () => {
+	cases.forEach((testCase, index) => {
+		it(`Case #${index + 1}`, () => {
+			assert.equal(fixName(testCase.input), testCase.expected);
+		});
+	});
 });
-
-describe('fixName compability', () => {
-	it('Case #1', () => {
-		assert.equal(fixName('Collector\'s Battalion\'s Backup Chemistry Set'), 'Collector\'s Battalion\'s Backup Chemistry Set');
-	})
-
-	it('Case #2', () => {
-		assert.equal(fixName('Strange Festivized Professional Killstreak Australium Scattergun'), 'Strange Festivized Professional Killstreak Australium Scattergun');
-	})
-
-	it('Case #3', () => {
-		assert.equal(fixName('Strange Omniscient Orb Balloonihoodie'), 'Strange Omniscient Orb Balloonihoodie');
-	})
-
-	it('Case #4', () => {
-		assert.equal(fixName('Hat #25'), 'Hat #25');
-	})
-
-	it('Case #5', () => {
-		assert.equal(fixName('Strange Unique Sniper Rifle'), 'Strange Unique Sniper Rifle');
-	})
-})
